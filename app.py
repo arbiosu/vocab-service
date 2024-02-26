9b5a899d5aeb8a508759f7e3f6248fe7d52a1f09
arbiosu <amea@oregonstate.edu>
2024-02-26 15:13:50 -0500

    flask app: receives JSON, executes sqlite query, returns JSON

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+@app.route('/query', methods=['POST'])
+def query_db():
+    list_id = request.json['list']
+    unit_id = request.json['unit']
+
+    # Connect to database
+    conn = sqlite3.connect('GRE_3333.db')
+    cur = conn.cursor()
+    # Construct SQLite query for GRE_3333.db
+    query = """
+    SELECT Vocabulary.word, Meaning.word_id, Meaning.meaning_US,
+    Meaning.sentence
+    FROM Meaning
+    INNER JOIN Vocabulary ON Meaning.word_id = Vocabulary.word_id
+    WHERE Meaning.list = ? AND Meaning.unit = ?
+    """
+
+    cur.execute(query, (list_id, unit_id))
+
+    result = cur.fetchall()
+    output = {}
+
+    for word, word_id, meaning_US, sentence in result:
+        output[word] = {
+            "word_id": word_id,
+            "meaning_US": meaning_US,
+            "sentence": sentence
+        }
+
+    conn.close()
+
+    return jsonify(output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
